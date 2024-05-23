1f544dd90b79037cef7b69e3829579e8eb59b03e
shaabane <shaabanalahmed20@gmail.com>
2024-05-23 15:00:09 +0200

    Installation de dependences et bootstrap

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+import 'jquery-nice-select/css/nice-select.css';
+import 'jquery-nice-select';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+import './styles/style.scss';
+import './styles/bootstrap.scss';
+import './styles/responsive.scss';
+import './styles/responsive.scss';
